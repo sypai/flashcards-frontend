eebfb724704e435599989f408085156e4f87637e
sypai <7suyashbajpai@gmail.com>
2020-04-05 00:27:48 +0530

    Frontend ready for flashcards

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+// var card = document.querySelector('.card-object');
+// var flipper = document.querySelector('.flip-it');
+
+function flipper(){
+    var card = document.querySelector('.card-object');
+    card.classList.toggle('is-flipped');
+};
